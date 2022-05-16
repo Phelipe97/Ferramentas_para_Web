a173e4c1042f54ca487cf72e60fdc96e6f3e48da
Phelipe97 <phelipe.ph25@gmail.com>
2022-05-16 18:33:55 -0300

    Ferramentas para desenvolvimento web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { familia } from './arquivo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'texte';
+  title = 'trabalho';
+  lista_familia = familia;
 }
---
+++ src/app/arquivo.ts
@@ -0,0 +1,34 @@
+
+export const familia = [
+{
+Nome: "Phelipe de Pontes Santos",
+RU: '3316094',
+Curso: "Analise e Desenvolvimento de Sistemas",
+Aniversario: "06/08/1997",
+},
+{
+Nome: "Raphael de Pontes Santos",
+RU: '1234567',
+Curso: "Design",
+Aniversario: "11/07/2006",
+},
+{
+Nome: "Gabriel de Pontes Santos",
+RU: '7654321',
+Curso: "Fundamental",
+Aniversario: "23/01/2016",
+},
+{
+Nome: "Claudio Roberto dos Santos",
+RU: '3456712',
+Curso: "Segurança do trabalho",
+Aniversario: "02/03/1973"
+},
+{
+Nome: "Maria Aparecida de Pontes Santos",
+RU: '7123456',
+Curso: "Teologia",
+Aniversario: "19/02/1972"
+}
+
+];
